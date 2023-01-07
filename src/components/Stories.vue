02806fb1064b0dd671065e083c7487335752d2e3
CoreyRobinsonDev <coreyrob52@gmail.com>
2023-01-07 13:15:22 -0500

    created story component

---
+++ src/components/Stories.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Story from './Story.vue';
+
+const res = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+const data = await res.json();
+</script>
+
+<template>
+  <Story v-for="value in data" :key="value" :id="value" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Story.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { id } = defineProps({
+  id: Number
+})
+
+const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`);
+const {title, by, kids, score, time} = await res.json();
+
+
+
+</script>
+
+<template>
+  <article>
+    <header>
+      <p>By: {{ by }}</p>
+      <p>{{ score }}</p>
+    </header>
+    <main>
+      <h2>{{ title }}</h2>
+    </main>
+  </article>
+</template>
+
+<style scoped>
+</style>
